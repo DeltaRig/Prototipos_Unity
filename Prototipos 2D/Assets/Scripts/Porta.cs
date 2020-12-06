19645ed07cb3123c304bddc95b0bbd11eca2a3d7
Filipe Johansson <hitomikaleiko@gmail.com>
2020-12-06 18:58:53 -0300

    P2D DoorAnimation

---
+++ Prototipos 2D/Assets/Scripts/Porta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta : MonoBehaviour
+{
+
+    public string texto;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>(); // pega componente animator do obj que o code pertence
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+	    if(collision.gameObject.tag == "Player"){
+            Debug.Log(texto);
+            animator.SetBool("canOpen", true);
+        }
+	}
+}
