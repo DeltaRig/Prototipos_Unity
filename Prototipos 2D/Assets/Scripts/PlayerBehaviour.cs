798488d104959c07a22b5ac1da8c6d2aaa9ca484
Filipe Johansson <hitomikaleiko@gmail.com>
2020-12-06 20:00:13 -0300

    P2D life and damage

--- Prototipos 2D/Assets/Scripts/PlayerBehaviour.cs
+++ Prototipos 2D/Assets/Scripts/PlayerBehaviour.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     private int speed = 4;
-    private bool isCollising = false;
+
+    public Image[] lifes = new Image[3];
+    [SerializeField]
+    private int life = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -17,20 +21,45 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement();
-
+    
     }
 
-    private void movement()
+	void FixedUpdate() {
+        movement();
+    }
+
+	private void movement()
     {
         float inputX = Input.GetAxis("Horizontal"); //puxa das config do unity
         float inputY = Input.GetAxis("Vertical");
 
-        if (!isCollising)
-        {
-            Vector2 movement = new Vector2(speed * inputX * Time.deltaTime, speed * inputY * Time.deltaTime);
-            transform.Translate(movement);
-        }
-        
+        Vector2 movement = new Vector2(speed * inputX * Time.deltaTime, speed * inputY * Time.deltaTime);
+        transform.Translate(movement);
     }
+
+    private void takeDamage(){
+        if(life > 0){
+            life--;
+            lifes[life].color = new Color32(255, 3, 0, 45);
+        }
+        // fazer morte
+	}
+
+    private void receiveLife(){
+        if(life < lifes.Length){
+            lifes[life].color = new Color32(255, 255, 255, 255);
+            life++;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+        var tag = collision.gameObject.tag;
+
+        if ( tag == "Damage"){
+            takeDamage();
+		} else if(tag == "Health"){
+            receiveLife();
+		}
+	}
+
 }
--- README.md
+++ README.md
@@ -6,10 +6,11 @@
 - Movimentação do player
 - Colisão com RigidBody
 - Animação da porta abrindo quando o player colide
+- Vida\[3\]
+- Recebe vida quando passa na poção
+- Dano para tag "Damage"
 
 *Objetivos para alcançar:*
 - Menuzinho inicial
-- Vida
-- Dano
 - GameOver
 - Fazer mapas a partir de imagens pixeladas
