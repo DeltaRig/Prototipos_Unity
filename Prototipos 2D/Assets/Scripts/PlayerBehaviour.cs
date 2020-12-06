a55622fe9751649859f3c557bad13fdf34b8d497
DeltaRig <hitomikaleiko@gmail.com>
2020-12-05 23:50:55 -0300

    P2D moving

---
+++ Prototipos 2D/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private int speed = 4;
+    private bool isCollising = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //vida, identificação de entidades
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+
+    }
+
+    private void movement()
+    {
+        float inputX = Input.GetAxis("Horizontal"); //puxa das config do unity
+        float inputY = Input.GetAxis("Vertical");
+
+        if (!isCollising)
+        {
+            Vector2 movement = new Vector2(speed * inputX * Time.deltaTime, speed * inputY * Time.deltaTime);
+            transform.Translate(movement);
+        }
+        
+    }
+}
