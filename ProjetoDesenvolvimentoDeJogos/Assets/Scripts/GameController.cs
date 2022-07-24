9c80b0c7121ece4fbfd0ff0e144b43236edfa2fd
DeltaRig <daniprigoli@gmail.com>
2022-07-24 20:53:56 -0300

    fix jump

--- ProjetoDesenvolvimentoDeJogos/Assets/Scripts/GameController.cs
+++ ProjetoDesenvolvimentoDeJogos/Assets/Scripts/GameController.cs
@@ -39,7 +39,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _gameTime += Time.deltaTime;
+        
     }
 
     // METODOS PLAYER
